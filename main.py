1a7db3208697dd3aa8201360701df4c5c0d9ab26
ssayyadi2009 <sayyadishervin@gmail.com>
2025-08-23 14:11:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -51,4 +51,4 @@ def webhook():
 # اجرای اپلیکیشن
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
